d14aba219e15322cabe853975c1f99962e662986
todzhang <todzhang@gmail.com>
2020-05-17 07:54:02 -0400

    import getopt

--- src/lcx.h
+++ src/lcx.h
@@ -1,7 +1,7 @@
 ﻿// lcx.h: 标准系统包含文件的包含文件
 // 或项目特定的包含文件。
 
-#ifndef LCX_H		/* only run through this once */
+#ifndef LCX_H /* only run through this once */
 #define LCX_H
 
 //ss#pragma once
@@ -12,30 +12,71 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <signal.h>
-#include  <netinet/in.h>
-#include  <arpa/inet.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <pthread.h>
 #include <netdb.h>
 
+#include <getopt.h>
+#include "LcxConfig.h"
 
-#define VERSION                        "1.00"
-#define TIMEOUT                        300
-#define MAXSIZE                        20480
-#define HOSTLEN                        40
-#define CONNECTNUM                     5
+#define TIMEOUT 300
+#define MAXSIZE 20480
+#define HOSTLEN 40
+#define CONNECTNUM 5
 
 //The following macro increased to modify,but i can't ensure they are correct. by:三只小潴
-#define INVALID_SOCKET                 -1
-#define SOCKET_ERROR                   -1
+#define INVALID_SOCKET -1
+#define SOCKET_ERROR -1
 typedef unsigned int UINT;
 //------------------------------------------------------------------------------------//
 
 // define 2 socket struct
 struct transocket
 {
-    int fd1;
-    int fd2;
+	int fd1;
+	int fd2;
 };
+
+typedef enum _METHOD
+{
+	LISTEN = 1,
+	TRAN,
+	SLAVE,
+	SSOCKSD,
+	RCSOCKS,
+	RSSOCKS,
+	NETCAT
+} METHOD;
+
+#define STR_LISTEN "listen"
+#define STR_TRAN "tran"
+#define STR_SLAVE "slave"
+#define STR_SSOCKSD "ssocksd"
+#define STR_RCSOCKS "rcsocks"
+#define STR_RSSOCKS "rssocks"
+#define STR_NETCAT "netcat"
+
+typedef struct _GlobalArgs
+{
+	METHOD method;		 /* -methon option */
+	char *logFileName; /* -o option */
+	FILE *logFile;
+	int verbosity; /* -v option */
+
+	int iListenPort;
+	char *listenHost;
+
+	int iConnectPort;
+	char *connectHost;
+
+	int iTransmitPort;
+	char *transmitHost;
+
+	int bFreeConsole;
+
+	int ssl;
+} GlobalArgs;
 
 // define function
 void ver();
@@ -44,13 +85,21 @@ void transmitdata(void *data);
 void getctrlc(int j);
 void closeallfd();
 void makelog(char *buffer, unsigned long length);
-void bind2bind(int port1, int port2);
-void bind2conn(int port1, char *host, int port2);
-void conn2conn(char *host1, int port1, char *host2, int port2);
+
+void bind2bind(GlobalArgs args);
+void bind2conn(GlobalArgs args);
+void conn2conn(GlobalArgs args);
 int testifisvalue(char *str);
 int create_socket();
 int create_server(int sockfd, int port);
-int client_connect(int sockfd, char* server, int port);
+int client_connect(int sockfd, char *server, int port);
+
+METHOD str2method(char *method);
+
+void ssocksd(GlobalArgs args);
+void rcsocks(GlobalArgs args);
+void rssocks(GlobalArgs args);
+void netcat(int argc, char **argv);
 
 /* ================ */
 #endif /* LCX_H */
